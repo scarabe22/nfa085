021528b8b9d1777729c44fce51abd5d0ab6ee3b2
scarabe22 <didiercorlay22@gmail.com>
2023-04-07 13:18:45 +0200

    ajout de exercice1.2 ébauche

--- docs/js/seance8/ex-js/exercices.js
+++ docs/js/seance8/ex-js/exercices.js
@@ -1,8 +1,9 @@
 /* background-color */
-let body = document.querySelector("body")
-let bg_orange = document.getElementById("btn1");
-let bg_greenyellow = document.getElementById("btn2")
-let bg_indianred = document.getElementById("btn3")
+// Changer la couleur de fond
+let body = document.querySelector("body"),
+   bg_orange = document.getElementById("btn1"),
+   bg_greenyellow = document.getElementById("btn2"),
+   bg_indianred = document.getElementById("btn3")
 bg_orange.addEventListener("click", function (){
    body.style.backgroundColor = "coral"
 });
@@ -12,3 +13,27 @@ bg_greenyellow.addEventListener("click", function (){
 bg_indianred.addEventListener("click", function (){
    body.style.backgroundColor = "indianred"
 });
+
+/* Add-select */
+// Fonction pour ajouter un élément à la liste
+let bt = document.getElementById("bt");
+bt.addEventListener("click", function (){ // Action de cliquer sur le bouton lié à une fonction
+
+   let value = document.getElementById("msg-input").value,// Récupération de la valeur saisie dans la zone de texte
+       list = document.getElementById("list"), //sélection de l'ul
+       li = document.createElement("li"),// Création d'un élément li
+       txNode = document.createTextNode(value),// Ajout de la valeur saisie dans l'élément
+
+   list.appendChild(li);// Ajout d'un élément li à la liste
+   li.appendChild(txNode);// Ajout du texte saisi dans le li de la liste
+
+});
+
+
+
+
+
+
+
+
+}
