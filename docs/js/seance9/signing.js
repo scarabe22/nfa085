4e84568f0a323e59c05e3f52973355be7dbca658
scarabe22 <didiercorlay22@gmail.com>
2023-04-19 22:04:45 +0200

    ajout de la check strength bar

--- docs/js/seance9/signing.js
+++ docs/js/seance9/signing.js
@@ -18,17 +18,69 @@ form.addEventListener("submit", function (event) {
     message.className = "message valid";
   }
 });
+
 mail.addEventListener("input", function (event) {
   if (mail.validity.valid) {
     message.innerHTML = "";
     message.className = "message";
-  }
+
+}
+
 });
 
-mail.addEventListener("change", function (event) {
-  if(mail.validity.typeMismatch) {
-    mail.setCustomValidity("Cet email n'en est pas un !");
-  } else {
-    mail.setCustomValidity("");
+/********************************************/
+//Validation du mot de passe avec check strength bar */
+//****************************************************/
+let parameters = {
+  count : false,
+  letters : false,
+  numbers : false,
+  special : false
+}
+let strengthBar = document.getElementById("strength-bar");
+let msg = document.getElementById("msg");
+
+let password = document.getElementById("pass1");
+password.addEventListener('input', (strengthChecker));
+
+function strengthChecker(){
+  let password = document.getElementById("pass1").value;
+
+  parameters.letters = (/[A-Za-z]+/.test(password))?true:false;
+  parameters.numbers = (/[0-9]+/.test(password))?true:false;
+  parameters.special = (/[!\"$%&/()=?@~`\\.\';:+=^*_-]+/.test(password))?true:false;
+  parameters.count = (password.length > 7)?true:false;
+
+  let barLength = Object.values(parameters).filter(value=>value);
+
+  console.log(Object.values(parameters), barLength);
+
+  strengthBar.innerHTML = "";
+  for( let i in barLength){
+    let span = document.createElement("span");
+    span.classList.add("strength");
+    strengthBar.appendChild(span);
   }
-});
+
+  let spanRef = document.getElementsByClassName("strength");
+  for( let i = 0; i < spanRef.length; i++){
+    switch(spanRef.length - 1){
+      case 0 :
+        spanRef[i].style.background = "#ff3e36";
+        msg.textContent = "Force: Très faible";
+        break;
+      case 1:
+        spanRef[i].style.background = "#ff691f";
+        msg.textContent = "Force: Faible";
+        break;
+      case 2:
+        spanRef[i].style.background = "#ffda36";
+        msg.textContent = "Force: Bon";
+        break;
+      case 3:
+        spanRef[i].style.background = "#0be881";
+        msg.textContent = "Force: Excellent";
+        break;
+    }
+  }
+}
