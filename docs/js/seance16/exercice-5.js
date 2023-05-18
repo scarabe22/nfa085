f94b5b5789d7487601959c73af856376e59c9329
scarabe22 <didiercorlay22@gmail.com>
2023-05-18 13:35:00 +0200

    suppression exercice-5.js

--- docs/js/seance16/exercice-5.js
+++
@@ -1,9 +0,0 @@
-$('.ui.dropdown').dropdown();
-
-// const showModal = document.querySelector(".ui.button") 
-$('.ui.button').click( () => {
-  $('.mini.ui.modal').modal('show');
-  $('.ui.dropdown').dropdown();
-});
-
-// $('.mini.modal').modal('show');
